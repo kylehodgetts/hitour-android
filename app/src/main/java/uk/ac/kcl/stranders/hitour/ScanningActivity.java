e7abef186d9512196b56e7bfbe157c410d495c1f
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2016-02-07 18:53:54 +0000

    Implemented manual code/pin entry on ScanningActivity

--- app/src/main/java/uk/ac/kcl/stranders/hitour/ScanningActivity.java
+++ app/src/main/java/uk/ac/kcl/stranders/hitour/ScanningActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -27,6 +29,24 @@ public class ScanningActivity extends AppCompatActivity {
         capture = new CaptureManager(this, barcodeScannerView);
         capture.initializeFromIntent(getIntent(), savedInstanceState);
         capture.decode();
+
+        Button btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText etCodePinEntry = (EditText) findViewById(R.id.etCodePinEntry);
+                // TODO: Needs to be changed when DB ready to search QR code data with DB and display relevant DetailActivity Page
+                if(etCodePinEntry.getText().toString().matches("\\d+")  && PrototypeData.containsId(Integer.parseInt(etCodePinEntry.getText().toString()))) {
+                    Intent intent = new Intent(ScanningActivity.this, DetailActivity.class);
+                    intent.putExtra(DetailActivity.EXTRA_BUNDLE, Integer.parseInt(etCodePinEntry.getText().toString()));
+                    startActivity(intent);
+                }
+                else {
+                    Log.d("FeedActivity", "Point for " + etCodePinEntry.getText() + " not found!");
+                    Toast.makeText(ScanningActivity.this, "Point Not Found, Please try again.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 
     @Override
