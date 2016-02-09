d740f774c0f2d60b7e3b4e7922fdcc6283d34f66
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2016-02-09 16:00:28 +0000

    Comments added to the ScanningActivity class

--- app/src/main/java/uk/ac/kcl/stranders/hitour/ScanningActivity.java
+++ app/src/main/java/uk/ac/kcl/stranders/hitour/ScanningActivity.java
@@ -18,12 +18,32 @@ import com.journeyapps.barcodescanner.CompoundBarcodeView;
 
 import java.util.List;
 
+/**
+ * {@link AppCompatActivity} class that is used to retrieve input by means of scanning a QR Code or
+ * manually entering a pin or point reference.
+ *
+ * This Activity contains a Barcode Scanner as well as an {@link EditText} to receive input
+ *
+ */
 public class ScanningActivity extends AppCompatActivity {
 
+    /**
+     * Field to store a reference to the BarcodeScanner embedded on the Activity
+     */
     private CompoundBarcodeView barcodeScannerView;
+
+    /**
+     * Field that stores a reference to the {@link EditText} field on the Activity
+     */
     private EditText etCodePinEntry;
 
 
+    /**
+     * Field to store a {@link BarcodeCallback} which handles what the barcode scanner should accept
+     * and what to do when it has detected an accepting barcode type.
+     *
+     * Where it will update the {@link EditText} field with the data it captured and execute the submit method
+     */
     private BarcodeCallback callback = new BarcodeCallback() {
         @Override
         public void barcodeResult(BarcodeResult result) {
@@ -41,6 +61,11 @@ public class ScanningActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * Creates an instance of the activity by telling barcode scanner to scan using the {@link BarcodeCallback}
+     * above and adding an {@link android.view.View.OnClickListener} to the Submit {@link Button}
+     * @param savedInstanceState - Saved Instance Bundle
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -59,6 +84,13 @@ public class ScanningActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Method that checks if the data input to the {@link EditText} field matches a valid format
+     * and that it matches a value stored for a point.
+     * Then navigates the use to the point data scanned.
+     *
+     * Otherwise an error message is shown to the user and the input is cleared ready for the next input.
+     */
     public void submit() {
         EditText etCodePinEntry = (EditText) findViewById(R.id.etCodePinEntry);
         // TODO: Needs to be changed when DB ready to search QR code data with DB and display relevant DetailActivity Page
@@ -77,39 +109,64 @@ public class ScanningActivity extends AppCompatActivity {
         }
     }
 
+    /**
+     * Clears input received in the {@link EditText} field and the Barcode Scanner's status bar text
+     */
     private void clearInput() {
         barcodeScannerView.setStatusText("");
         etCodePinEntry.setText("");
     }
 
+    /**
+     * Resumes the {@link AppCompatActivity} and resumes the Barcode Scanner
+     */
     @Override
     protected void onResume() {
         super.onResume();
         barcodeScannerView.resume();
     }
 
+    /**
+     * Pauses the {@link AppCompatActivity} and pauses the barcode scanner and camera input
+     */
     @Override
     protected void onPause() {
         super.onPause();
         barcodeScannerView.pause();
     }
 
+    /**
+     * Destroys the instance of the {@link AppCompatActivity}
+     */
     @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
+    /**
+     * Saves the state of the {@link AppCompatActivity}
+     */
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
     }
 
+    /**
+     * Deals with when it is navigated up of the {@link AppCompatActivity}
+     */
     @Override
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
     }
 
+    /**
+     * Deals with certain key presses when using the Barcode Scanner
+     *
+     * @param keyCode Keys pressed
+     * @param event Event
+     * @return boolean if keys are pressed
+     */
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
         return barcodeScannerView.onKeyDown(keyCode, event) || super.onKeyDown(keyCode, event);
