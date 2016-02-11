f04283b56f63d596322f584b147124866d203612
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2016-02-11 10:45:39 +0000

    Add comments to DetailFragement and DetailFragementTest classes

--- app/src/androidTest/java/uk/ac/kcl/stranders/hitour/DetailFragmentTest.java
+++ app/src/androidTest/java/uk/ac/kcl/stranders/hitour/DetailFragmentTest.java
@@ -8,18 +8,32 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
-
+/**
+ * Front end instrumentation test for the {@link DetailFragment} ensuring that its content is correctly
+ * populated and the video behaves as it should when interacted with.
+ *
+ */
 public class DetailFragmentTest extends ActivityInstrumentationTestCase2<DetailActivity> {
 
-
+    /**
+     * Default constructor to set up the test for the {@link DetailActivity} which hosts the
+     * {@link DetailFragment}
+     */
     public DetailFragmentTest() {
         super(DetailActivity.class);
     }
 
+    /**
+     * Test to check the activity is correctly started and exists.
+     */
     public void testActivityExists() {
         assertNotNull(getActivity());
     }
 
+    /**
+     * Test that all of the content for the point has been loaded correctly on the fragment and
+     * that they all exist.
+     */
     public void testFragmentViewsExist() {
         TextView titleView = (TextView) getActivity().findViewById(R.id.text_title);
         TextView bodyView = (TextView) getActivity().findViewById(R.id.text_body);
@@ -32,6 +46,10 @@ public class DetailFragmentTest extends ActivityInstrumentationTestCase2<DetailA
         assertNotNull(videoView);
     }
 
+    /**
+     * Tests that the video is correctly sized and positioned to be the whole width of the the
+     * {@link DetailFragment} and the height wrapped to its content.
+     */
     public void testVideoSize() {
         VideoView videoView = (VideoView) getActivity().findViewById(Integer.parseInt("1"));
         LinearLayout linearLayout = (LinearLayout) getActivity().findViewById(R.id.detail_body);
@@ -40,6 +58,11 @@ public class DetailFragmentTest extends ActivityInstrumentationTestCase2<DetailA
         assertEquals(linearLayout.getWidth(), videoView.getWidth());
     }
 
+    /**
+     * Tests that the video behaves as it should when playing and pausing. Which checks on a touch if
+     * the video is playing it will pause. If it is touched and the video is not playing then it will
+     * start to play.
+     */
     public void testVideoPlayPause() {
         VideoView videoView = (VideoView) getActivity().findViewById(Integer.parseInt("1"));
         getInstrumentation().waitForIdleSync();
@@ -53,6 +76,10 @@ public class DetailFragmentTest extends ActivityInstrumentationTestCase2<DetailA
         assertEquals(false, videoView.isPlaying());
     }
 
+    /**
+     * Tests that that video when resumed, starts playing from the last point it left off and
+     * not returning back to the beginning.
+     */
     public void testVideoResume() {
         VideoView videoView = (VideoView) getActivity().findViewById(Integer.parseInt("1"));
         getInstrumentation().waitForIdleSync();
@@ -67,6 +94,10 @@ public class DetailFragmentTest extends ActivityInstrumentationTestCase2<DetailA
         assertTrue(videoView.getCurrentPosition() > 0);
     }
 
+    /**
+     * Tests that if the device is rotated the video will resume playing from the last point
+     * it left off before the rotation occurred rather than start again from the beginning.
+     */
     public void testVideoResumeOnRotation() {
         VideoView videoView = (VideoView) getActivity().findViewById(Integer.parseInt("1"));
         TouchUtils.clickView(this, videoView);
--- app/src/main/java/uk/ac/kcl/stranders/hitour/DetailFragment.java
+++ app/src/main/java/uk/ac/kcl/stranders/hitour/DetailFragment.java
@@ -15,20 +15,63 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+/**
+ * Fragment that shows the content for a particular point in the tour which could consist of
+ * text, images, videos or any number of combinations between them.
+ *
+ * That pulls the required data from local storage and displays in an ordered list using this fragment.
+ *
+ */
 public class DetailFragment extends Fragment {
 
+    /**
+     * Static String to name to store in a bundle the item's ID
+     */
     public static final String ARG_ITEM_ID = "ITEM_ID";
+
+    /**
+     * Stores the integer ID of the current item selected to view
+     */
     private int mItemId;
+
+    /**
+     * Stores the root view where the fragment is inflated to
+     */
     private View mRootView;
+
+    /**
+     * Stores the main image shown for the point
+     */
     private ImageView mImageView;
+
+    /**
+     * Stores the video to be shown for the point
+     */
     private VideoView videoView;
+
+    /**
+     * Stores the cursor to navigate the around the prototype data
+     */
     private Cursor mCursor;
+
+    /**
+     * Stores the current position of the video
+     */
     private int currentPosition;
 
+    /**
+     * Default empty required public constructor
+     */
     public DetailFragment() {
         // Required empty public constructor
     }
 
+    /**
+     * Creates a new instance of a fragment for the specified item id
+     *
+     * @param itemId Integer item id
+     * @return {@link Fragment}
+     */
     public static DetailFragment newInstance(int itemId) {
         Bundle arguments = new Bundle();
         arguments.putInt(ARG_ITEM_ID, itemId);
@@ -37,6 +80,10 @@ public class DetailFragment extends Fragment {
         return fragment;
     }
 
+    /**
+     * Set's up the {@link Fragment}'s data ready for it's views to be created
+     * @param savedInstanceState
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -49,7 +96,15 @@ public class DetailFragment extends Fragment {
         }
     }
 
-
+    /**
+     * Creates and inflates the view's on the {@link Fragment} from the data for the selected point
+     * including its images, text and videos.
+     *
+     * @param inflater {@link LayoutInflater}
+     * @param container {@link ViewGroup} of where the views are to be created into
+     * @param savedInstanceState {@link Bundle} with all the saved state variables
+     * @return {@link View} Fragment containing all of its views
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              final Bundle savedInstanceState) {
@@ -121,12 +176,24 @@ public class DetailFragment extends Fragment {
         return mRootView;
     }
 
+    /**
+     * Saves the current state of the fragment and saves the current position of the video used when
+     * the fragment is paused.
+     *
+     * @param outState {@link Bundle}
+     */
     @Override
     public void onSaveInstanceState(Bundle outState) {
         outState.putInt("currentPosition", currentPosition);
         super.onSaveInstanceState(outState);
     }
 
+    /**
+     * Restores the state of the fragment from the {@link Bundle} and restores the video's positions
+     * to its last position
+     *
+     * @param savedInstanceState {@link Bundle}
+     */
     @Override
     public void onViewStateRestored(Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
@@ -135,6 +202,9 @@ public class DetailFragment extends Fragment {
         }
     }
 
+    /**
+     * Pauses the fragment and stores the current position of the video.
+     */
     @Override
     public void onPause() {
         super.onPause();
