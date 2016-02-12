e885da0600fbacdf3893209c084b3db4f636694f
Charlie Baker <Charlie.Baker@kcl.ac.uk>
2016-02-12 07:40:42 +0000

    Amend prototype data and video files

--- app/src/main/java/uk/ac/kcl/stranders/hitour/PrototypeData.java
+++ app/src/main/java/uk/ac/kcl/stranders/hitour/PrototypeData.java
@@ -13,16 +13,13 @@ public class PrototypeData {
     public static Cursor getCursor() {
         String[] columns = new String[] { "_id", "title", "description", "image", "video"};
         MatrixCursor cursor = new MatrixCursor(columns);
-        cursor.addRow(new Object[] { 0, "Magnetic Resonance Imaging Scanner", "Magnetic resonance imaging (MRI) is a noninvasive medical test that physicians use to diagnose and treat medical conditions.\n" +
-                "\n" +
-                "MRI uses a powerful magnetic field, radio frequency pulses and a computer to produce detailed pictures of organs, soft tissues, bone and virtually all other internal body structures. MRI does not use ionizing radiation (x-rays).\n" +
-                "\n" +
-                "Detailed MR images allow physicians to evaluate various parts of the body and determine the presence of certain diseases. The images can then be examined on a computer monitor, transmitted electronically, printed or copied to a CD.", R.drawable.mri, R.raw.mriscan});
+        cursor.addRow(new Object[] { 0, "Magnetic Resonance Imaging Scanner", "Magnetic resonance imaging (MRI) is a noninvasive medical test that physicians use to diagnose and treat medical conditions. MRI uses a powerful magnetic field, radio frequency pulses and a computer to produce detailed pictures of organs, soft tissues, bone and virtually all other internal body structures. MRI does not use ionizing radiation (x-rays). \nDetailed MR images allow physicians to evaluate various parts of the body and determine the presence of certain diseases. The images can then be examined on a computer monitor, transmitted electronically, printed or copied to a CD.", R.drawable.mri, R.raw.mriscan});
         cursor.addRow(new Object[] { 1, "Fluoroscopy Suite", "fluoroscopy is a radiology technique that takes a real time \"movie\" of the body. A continuous X-ray beam is passed through the body part being examined and is transmitted to a TV-like monitor so that the body part and its motion can be seen in detail.", R.drawable.fluoroscopy, R.raw.fluoroscopy});
         cursor.addRow(new Object[] { 2, "Computed tomography", "Computed tomography (CT) is a diagnostic imaging test used to create detailed images of internal organs, bones, soft tissue and blood vessels. \n\nThe cross-sectional images generated during a CT scan can be reformatted in multiple planes, and can even generate three-dimensional images which can be viewed on a computer monitor, printed on film or transferred to electronic media. \n\nCT scanning is often the best method for detecting many different cancers since the images allow your doctor to confirm the presence of a tumor and determine its size and location. CT is fast, painless, noninvasive and accurate. In emergency cases, it can reveal internal injuries and bleeding quickly enough to help save lives.", R.drawable.ctscan, R.raw.ctscan});
         cursor.addRow(new Object[] { 3, "Ultrasound scan", "An ultrasound scan, sometimes called a sonogram, is a procedure that uses high-frequency sound waves to create an image of part of the inside of the body.", R.drawable.ultrasound, R.raw.ultrasound});
 
         return cursor;
     }
+
 
 }
